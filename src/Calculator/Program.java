0595642500bad7cc4c833f0281d5371284099fe2
Amletick <VadimV2001@gmail.com>
2024-02-27 23:49:57 +0300

    Sim2HW1.3(CalcDone)

--- src/Calculator/Program.java
+++ src/Calculator/Program.java
@@ -2,11 +2,15 @@ package Calculator;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.math.*;
+
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
 
 public class Program {
     public static void main(String ... args){
         Calculator calc = new Calculator();
-        System.out.println(calc.calculate("/",4,7));
+        System.out.println(calc.calculate("√",27,3));
     }
 }
 
@@ -16,34 +20,21 @@ class Calculator{
         fill();
     }
     private void fill(){
-        map.put("+", new Addition());
+        map.put("+",(b)-> b[0]+b[1]);
         map.put("-",(b)-> b[0]-b[1]);
         map.put("*",(b)-> b[0]*b[1]);
+        map.put("/",(b)-> b[0]/b[1]);
+        map.put("^",(b)->pow(b[0],b[1]));
+        map.put("2√",(b)->sqrt(b[0]));//Впринципе не нужен, просто для практики
+        map.put("√",(b)->pow(b[0],1/b[1]));
     }
-    public double add(int a, int b){
-        return a+b;
-    }
-    public double subtract(int a, int b){
-        return a-b;
-    }
-    public double multiply(int a, int b){
-        return a*b;
-    }
+
     public double calculate(String operator,int a, int b){
         return map.get(operator).calculate(a,b);
     }
 }
 
-class Addition implements Calculable{
 
-    @Override
-    public double calculate(double... args) {
-        if (args.length<2){
-            throw new RuntimeException("Мало аргументов");
-        }
-        return args[0]+args[1];
-    }
-}
 interface Calculable{
     double calculate(double ... args);//double ... args==String[] args
 }
